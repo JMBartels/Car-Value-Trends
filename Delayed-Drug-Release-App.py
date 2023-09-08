c9a570722900dd2505cf8605c9ecd100d2aa755c
JMBartels <142684852+JMBartels@users.noreply.github.com>
2023-09-08 08:58:40 -0400

    Update Delayed-Drug-Release-App.py with server ref

--- Delayed-Drug-Release-App.py
+++ Delayed-Drug-Release-App.py
@@ -63,6 +63,7 @@ def ConcProfile(MW, th_init, conc):
 #Interactive Dashboard that recalculates and plots ConcProfile
 # ___MAIN___
 app = Dash(__name__)
+server = app.server
 
 app.layout = html.Div([
     html.Div(children=[
